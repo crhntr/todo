e1859a82258f2927acab72169ab4d20a08f1b557
Christopher Hunter <hunter.christopher@icloud.com>
2020-09-25 21:25:18 -0700

    setup todo template in html

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/crhntr/todo
 go 1.14
 
 require (
+	github.com/crhntr/window v0.0.0-20200915012401-3936071b466a
 	github.com/go-git/go-billy/v5 v5.0.0
 	github.com/julienschmidt/httprouter v1.3.0
 	go.mongodb.org/mongo-driver v1.4.1
--- pages/todo/main.go
+++ pages/todo/main.go
@@ -1,9 +1,49 @@
+// +build js,wasm
+
 package main
 
 import (
+	"bytes"
 	"fmt"
+
+	"github.com/crhntr/window"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"github.com/crhntr/todo"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	tmp, err := window.LoadTemplates(nil, `script[type="text/go-template"]`)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	tasks := []todo.Task{
+		{
+			ID:    primitive.NewObjectID(),
+			Title: "Task 1",
+		},
+		{
+			ID:    primitive.NewObjectID(),
+			Title: "Task 2",
+		},
+		{
+			ID:    primitive.NewObjectID(),
+			Title: "Task 3",
+		},
+	}
+
+	tasksEL := window.Document.QuerySelector(".tasks")
+	var buf bytes.Buffer
+	for _, task := range tasks {
+		buf.Reset()
+
+		if err := tmp.ExecuteTemplate(&buf, "task", task); err != nil {
+			fmt.Printf("failed to render task %s: %s", task.ID, err)
+			continue
+		}
+
+		tasksEL.AppendHTML(buf.String())
+	}
 }
